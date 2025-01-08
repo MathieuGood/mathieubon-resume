df5d802d339a127f6021b5bad4444282fa7c028c
Mathieu <bon.mathieu@gmail.com>
2025-01-08 20:14:53 +0100

    Deleted background color on div for debugging.

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -7,11 +7,13 @@ import LeftColumn from "./LeftColumn"
 
 const Resume: React.FC = () => {
 	return (
-		<div className="resume page flex">
-			<div className="left-column w-1/3 h-screen bg-custom-yellow">
+		<div className="resume page flex h-max">
+			<div className="left-column w-1/3 h-full bg-white">
 				<LeftColumn />
 			</div>
-			<div className="main-column w-2/3 h-screen bg-red-300">
+			<div
+				// TODO:  Add bg-red-600 to the className to see what zone is overlapping
+				className="main-column w-2/3 h-full bg-[white]">
 				<div className="hero bg-custom-gray h-36 pr-8 py-6 font-mono text-right text-custom-yellow">
 					<h1 className=" text-5xl mb-2">Mathieu Bon</h1>
 					<p className="text-xl">Consultant Développeur</p>
